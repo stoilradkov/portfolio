2b2dd17dcf59a614c01432f011eb3fa1ce0ef6dc
Stoil Radkov <stoil.radkov@schibsted.com>
2022-08-09 16:45:26 +0300

    Setup lighthouse

---
+++ lighthouserc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  ci: {
+    collect: {
+      numberOfRuns: 3,
+      startServerCommand: 'serve:lhci',
+      url: ['http://localhost:3000/portfolio'],
+      settings: {
+        onlyCategories: [
+          'performance',
+          'accessibility',
+          'best-practices',
+          'seo',
+        ],
+        skipAudits: ['uses-http2'],
+        chromeFlags: '--no-sandbox',
+      },
+    },
+    assert: {
+      assertions: {
+        'categories:performance': [
+          'warn',
+          { minScore: 0.9, aggregationMethod: 'pessimistic' },
+        ],
+        'categories:accessibility': [
+          'error',
+          { minScore: 0.9, aggregationMethod: 'pessimistic' },
+        ],
+        'categories:best-practices': [
+          'error',
+          { minScore: 0.9, aggregationMethod: 'pessimistic' },
+        ],
+        'categories:seo': [
+          'error',
+          { minScore: 0.9, aggregationMethod: 'pessimistic' },
+        ],
+      },
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+}
