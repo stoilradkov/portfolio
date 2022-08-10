cfda31eefe17e5bed40cda48480611e76eb6124e
Stoil Radkov <stoil.radkov@schibsted.com>
2022-08-10 23:57:42 +0300

    Add social media accounts

--- src/home/Introduction.tsx
+++ src/home/Introduction.tsx
@@ -1,14 +1,33 @@
+import linkedin from '../assets/linkedin.png'
+import github from '../assets/github.png'
+
 const Introduction: React.FC = () => {
   return (
-    <article className="self-center">
+    <article className="self-end">
       <h2 className="mb-3 text-7xl font-semibold ">Hello! I am</h2>
       <h1 className="text-7xl font-semibold mb-8 text-primary-main">
         Stoil Radkov
       </h1>
-      <p className="text-text-main text-xl">
+      <p className="text-text-main text-xl mb-8">
         A software enginner specializing in full stack web development as well
         as cloud applications.
       </p>
+      <div aria-roledescription="Social media accounts" className=" flex gap-4">
+        <a
+          target="_blank"
+          href="https://www.linkedin.com/in/stoil-radkov-278361156/"
+          className="inline-block"
+        >
+          <img className="w-12 h-12" src={linkedin} alt="linkedin logo" />
+        </a>
+        <a
+          target="_blank"
+          href="https://github.com/stoilradkov"
+          className="inline-block"
+        >
+          <img className="w-12 h-12" src={github} alt="github logo" />
+        </a>
+      </div>
     </article>
   )
 }
