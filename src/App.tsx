2d52b9ab06ee982e708739601bb55a66aed16c7b
Stoil Radkov <stoil.radkov@schibsted.com>
2022-08-10 23:28:04 +0300

    Add not found page

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,22 @@ import Education from './education/Education'
 import Experience from './experience/Experience'
 import Home from './home/Home'
 import Navbar from './navbar/Navbar'
+import NotFound from './notFound/NotFound'
 
 function App() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/experience" element={<Experience />} />
-        <Route path="/education" element={<Education />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <main className="px-10">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/education" element={<Education />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
     </BrowserRouter>
   )
 }
