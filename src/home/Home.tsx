e5c047afccd09ef91461d022e86ab39096d87957
Stoil Radkov <stoil.radkov@schibsted.com>
2022-09-09 17:39:30 +0300

    Center spinner

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -8,7 +8,7 @@ const Home: React.FC = () => {
   return (
     <section className="grid grid-cols-1 lg:grid-cols-[2fr_3fr] gap-10 mb-8 overflow-hidden lg:overflow-visible">
       <Introduction />
-      <article className="place-self-center w-[666px] h-[375px] lg:w-auto lg:h-auto">
+      <article className="place-self-center flex justify-center items-center w-[666px] h-[375px] lg:w-auto lg:h-auto">
         <Suspense fallback={<Spinner />}>
           <Hero />
         </Suspense>
